b69d3c800bc083724706a8fe30a3cbb04bf098e7
liyancode <liyan.code@gmail.com>
2018-04-16 08:28:23 +0800

    prod webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -37,7 +37,11 @@ module.exports={
                     'url-loader?limit=10000',
                     'img-loader'
                 ]
-            }
+            },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },  //添加
+    { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" }, //添加
+    { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" }, //添加
+    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" } //添加
         ]
     },
     plugins:[
